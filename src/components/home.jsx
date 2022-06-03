0d97101b3cd3f056a226d87ded4858404f6e18d2
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-06-03 11:54:18 -0300

    Ajustes layout

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,7 @@
+import { useTranslation } from "react-i18next";
+
 export default function Home() {
-  return <div>Página Inicial</div>;
+  const { t, i18n } = useTranslation();
+
+  return <div>{t("app.home")}</div>;
 }
